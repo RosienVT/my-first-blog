bc6ee9a71184b88963455e55752ff929bffbf799
RosienVT <rosien.vantoor@gmail.com>
2016-06-01 17:07:34 +0200

    Added confirmation for deletion of posts and comments

---
+++ blog/templates/blog/Confirm.js
@@ -0,0 +1,12 @@
+/**
+ * Created by rosien on 1-6-16.
+ */
+
+var confirmation = confirm("Are you sure you want to delete?");
+if (confirmation == true) {
+//    delete the post and continue to main page
+    window.location
+} else {
+    //retun to post detail
+}
+
